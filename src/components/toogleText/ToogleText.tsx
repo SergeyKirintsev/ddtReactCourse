e4ccf0735dadde6a72b07aed5075449f9aae8eab
Sergey Kirintsev <kserg80@gmail.com>
2022-01-12 16:04:12 +0300

    fix: setIsShow

--- src/components/toogleText/ToogleText.tsx
+++ src/components/toogleText/ToogleText.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "./ToogleText.css";
 
 function ToogleText({ text }: { text: string }) {
-  const [isShow, setShow] = useState(false);
+  const [isShow, setIsShow] = useState(false);
 
   return (
     <div className="toogle">
       <button
         className="toogle__button"
         type="button"
-        onClick={() => setShow((prev) => !prev)}
+        onClick={() => setIsShow((prev) => !prev)}
       >
         {isShow ? "Скрыть текст" : "Показать текст"}
       </button>
